2cd3dad2bd00ef26dd4b1f92c57e293ef9d93493
Genesis Guerrero Martinez <genesisdaft@gmail.com>
2016-09-13 00:08:20 -0500

    listo

--- app.js
+++ app.js
@@ -17,35 +17,28 @@ var slots = [
   }
 ];
 
+var insertCoinSound = new Audio('./sounds/insert_coin.mp3');
+var cashOutSound = new Audio('./sounds/cashOut.mp3');
+var rollSound = new Audio('./sounds/roll.mp3');
+
 var vm = new Vue({
   el: '#app',
   data: {
     options: slots,
     credits: 50,
     bet: 1,
-    slot1: Math.floor(Math.random()*slots.length),
-    slot2: Math.floor(Math.random()*slots.length),
-    slot3: Math.floor(Math.random()*slots.length),
     handle: false,
+    isRunning: false,
     win: false
   },
   methods : {
-    shuffle: function(event) {
-      if(this.bet == 0){
-        return;
-      }
-      //Animation
-      this.handle = true;
+    finish: function(slotElements){
       var self = this;
-      
-      setTimeout(function() { self.handle = false; }, 500);
 
-      this.slot1 = Math.floor(Math.random()*slots.length);
-      this.slot2 = Math.floor(Math.random()*slots.length);
-      this.slot3 = Math.floor(Math.random()*slots.length);
-      if(this.slot1 == this.slot2 && this.slot1 == this.slot3){
-        this.credits += this.options[this.slot1].points * this.bet ; 
+      if(slotElements[0].active == slotElements[1].active && slotElements[0].active == slotElements[2].active){
+        this.credits += this.options[slotElements[0].active].points * this.bet ; 
         this.win = true;
+        cashOutSound.play();
         var count = 0;
         var intv = setInterval(function(){
           if (count == 4) clearInterval(intv);
@@ -61,11 +54,51 @@ var vm = new Vue({
       } else {
         this.bet = 0;
       }
+      this.isRunning = false;
+    },
+    shuffle: function(event) {
+      if(this.bet == 0 || this.isRunning){
+        return;
+      }
+      this.isRunning = true;
+      rollSound.play();
+
+      //Animation
+      this.handle = true;
+      var self = this;
+      setTimeout(function() { self.handle = false; }, 300);
+
+      var slotElements = [];
+      slotElements.push($("#slot1").slotMachine());
+      slotElements.push($("#slot2").slotMachine());
+      slotElements.push($("#slot3").slotMachine());
+
+      var count = 0;
+      var intv = setInterval(function(){
+        if (count == 3){
+          clearInterval(intv); 
+          count = 0;
+          var intv2 = setInterval(function(){
+            if (count == 3){
+              clearInterval(intv2); 
+              self.finish(slotElements);
+              return;
+            } 
+            slotElements[count].stop(); 
+            count++;
+          }, 600);
+          return;
+        } 
+        slotElements[count].shuffle(); 
+        count++;
+      }, 300);
+      
     }, 
     betOne: function(event) {
       if(this.credits > 0 && this.bet < 5) {
         this.bet++;
         this.credits--;
+        insertCoinSound.play();
       }
     }
   }
